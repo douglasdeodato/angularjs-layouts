5281a5a94d78bd2db0cff8748c320ea64b39fe77
Douglas Deodato <ddeodato.e@interactiveservices.com>
2015-10-01 16:35:52 +0100

    controller, ng repeat

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Download angular: https://angularjs.org/
 
 ## NG
  * ng-include
+ * ng-controller
+ * ng-repeat
---
+++ public/javascripts/ng-controller.js
@@ -0,0 +1,15 @@
+angular.module('myApp', [])
+.controller('UsersListCtrl',['$scope',function($scope){
+	    $scope.users =[
+ 		{name:'douglas', id:3},
+ 		{name:'bruno', id:1}
+ 	];
+}])
+
+.controller('StreetListCtrl',['$scope',function($scope){
+	    $scope.street =[
+ 		{name:'1 smithfield', id:3},
+ 		{name:'2 river plate', id:1333},
+ 		{name:'3 river plate', id:1}
+ 	];
+}])
